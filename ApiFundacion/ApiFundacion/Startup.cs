d0acda20c2dbc39c9973f69c8f4f50c104758e6c
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2022-01-22 15:07:23 -0300

    get areas

--- ApiFundacion/ApiFundacion/Startup.cs
+++ ApiFundacion/ApiFundacion/Startup.cs
@@ -1,3 +1,6 @@
+using ApiFundacion.Models;
+using ApiFundacion.Repository;
+using ApiFundacion.Repository.IRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +14,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiFundacion.Models.DTO;
 
 namespace ApiFundacion
 {
@@ -28,12 +33,23 @@ namespace ApiFundacion
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<dena66utud3alcContext>(option =>
+            option.UseNpgsql(Configuration.GetConnectionString("DefaultConnectionString")));
+
+
+            services.AddAutoMapper(configuration => {
+                configuration.CreateMap<Area, AreasDTO>();
+                configuration.CreateMap<AreasDTO, Area>();
+            }, typeof(Startup));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiFundacion", Version = "v1" });
             });
+
+            services.AddTransient<IAreaRepository, AreaRepository>();
+            //services.AddScoped<IAreaRepository, AreaRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
