e86181191d6b4eac347dde96f475efa627ba65dc
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2022-01-22 16:32:45 -0300

    departamentos

---
+++ ApiFundacion/ApiFundacion/Repository/Departamentos/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFundacion.Repository.Departamentos
+{
+    public class Class
+    {
+    }
+}
