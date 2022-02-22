9d2f9a90244edc2eaac5c4f5eeaf293b5db2cae8
lautaro disbroiavacca <111992@tecnicatura.frc.utn.edu.ar>
2022-02-22 11:46:33 -0300

    get publicaciones

---
+++ ApiFundacion/ApiFundacion/Repository/Publicaciones/IPublicacionesRepository.cs
@@ -0,0 +1,14 @@
+﻿using ApiFundacion.Models;
+using ApiFundacion.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFundacion.Repository.Publicaciones
+{
+    public interface IPublicacionesRepository
+    {
+        Task<List<Publicacionesxproyecto>> GetPublicaciones();
+    }
+}
---
+++ ApiFundacion/ApiFundacion/Repository/Publicaciones/PublicacionesRepository.cs
@@ -0,0 +1,24 @@
+﻿using ApiFundacion.Models;
+using ApiFundacion.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFundacion.Repository.Publicaciones
+{
+    public class PublicacionesRepository : IPublicacionesRepository
+    {
+        private readonly dena66utud3alcContext context;
+        public PublicacionesRepository(dena66utud3alcContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Publicacionesxproyecto>> GetPublicaciones()
+        {
+            return await context.Publicacionesxproyectos.ToListAsync();
+        }
+    }
+}
